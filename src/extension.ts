6a0c3c1964760d86bcd23b5e5a0785ad85373e43
Canklot <mustafacandan211@gmail.com>
2025-01-07 00:12:36 +0300

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,44 @@
 import * as vscode from 'vscode';
 import { readFileSync, appendFileSync } from 'fs';
 
+let configName = '.order';
+let stringToAdd = "-a";
+
+
+function changeSortOrder() {
+    const workspaceConfig = vscode.workspace.getConfiguration('explorer');
+    workspaceConfig.update('sortOrder', 'modified', vscode.ConfigurationTarget.Workspace);
+}
+
+function getConfig(): string[] {
+    let customOrderPath = getProjectPath() + configName;
+    let fileContent = readFileSync(customOrderPath, 'utf-8');
+    const lines = fileContent.split(/\r?\n/); // Handles both Windows and Unix line endings
+    lines.reverse();
+    return lines;
+}
+
+function getProjectPath(): string {
+    let workspaceUriPath = vscode.workspace.workspaceFolders?.map(folder => folder.uri.path).at(0);
+    if (!workspaceUriPath) { throw new URIError("No workspace detected"); }
+    let workspacePath = workspaceUriPath.slice(1);
+    workspacePath = workspacePath + "/";
+    return workspacePath;
+}
+
+function appendStringToFiles(fileList:string[],stringToAdd:string): void {
+    let workspacePath = getProjectPath();
+    for (let filename of fileList) {
+        console.log(filename);
+        let filePath = workspacePath + filename;
+        appendFileSync(filePath, stringToAdd, 'utf8');
+    }
+}
+
 export function activate(context: vscode.ExtensionContext) {
     vscode.workspace.onDidSaveTextDocument((document) => {
-        let stringToAdd = "-a";
-        let customOrderFileName = '.order';
-
-        let workspaceUriPath = vscode.workspace.workspaceFolders?.map(folder => folder.uri.path).at(0);
-        if (!workspaceUriPath) { return; }
-        let workspacePath = workspaceUriPath.slice(1);
-        workspacePath = workspacePath + "/";
-        let customOrderPath = workspacePath + customOrderFileName;
-        let fileContent = readFileSync(customOrderPath, 'utf-8');
-        const lines = fileContent.split(/\r?\n/); // Handles both Windows and Unix line endings
-        lines.reverse();
-        for (let line of lines) {
-            console.log(line);
-            let toOrderPath = workspacePath + line;
-            appendFileSync(toOrderPath, stringToAdd, 'utf8');
-        }
-
+        let fileOrder = getConfig();
+        appendStringToFiles(fileOrder,stringToAdd);
         console.log("re order completed");
     });
 }
