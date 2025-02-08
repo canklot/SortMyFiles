d2835ac9d8b2aedb505c9956eb531da7a106a880
Canklot <mustafacandan211@gmail.com>
2025-02-08 23:30:13 +0300

    fix default ignore pattern

--- src/findFiles.ts
+++ src/findFiles.ts
@@ -4,12 +4,13 @@ import { getProjectPath, prefixWithProjectPath } from './projectPath';
 import { minimatch } from "minimatch";
 
 export function getGitignoreFiles(): string {
+    let defaultGitignorePattern = '**/.git/**';
     try {
         let workspacePath = getProjectPath();
         let gitignorePath = workspacePath + '.gitignore';
         let fileContent = readFileSync(gitignorePath, 'utf-8');
         let lines = fileContent.split(/\r?\n/); // Handles both Windows and Unix line endings
-        lines.push('.git/'); // Normally gitignore files doesnt include the .git folder, but we want to include it
+        lines.push(defaultGitignorePattern); // Normally gitignore files doesnt include the .git folder, but we want to include it
         let removedComments = lines.filter(item => item.charAt(0) !== '#');
         let removedEmptyLines = removedComments.filter(item => item !== '');
         let prefixedGitignoreFiles = removedEmptyLines.map(file => "**/" + file + "**");
@@ -18,7 +19,8 @@ export function getGitignoreFiles(): string {
         ignorePatterns = `{${ignorePatterns}}`;
         return ignorePatterns;
     } catch (error) {
-        return "";
+        // if there is no gitignore file, return the default gitignore pattern
+        return defaultGitignorePattern;
     }
 }
 
@@ -29,11 +31,10 @@ export async function findAllFilesAndFoldersWithIgnore(folderUri: vscode.Uri, re
     for (const [entryName, entryType] of entries) {
         const entryUri = vscode.Uri.joinPath(folderUri, entryName);
         // Check if the current folder fits the ignore pattern
-        if (minimatch(entryUri.fsPath, ignorePattern)) {
+        if (minimatch(entryUri.path, ignorePattern)) {
             continue;
         }
         // Add the folder URI to the set
-        // remove the first slash from the uri
         resultSet.add(entryUri.fsPath);
         if (entryType === vscode.FileType.Directory) {
             // If result is a directory, recursively call the function
